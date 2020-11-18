c5dcbbe20bf43e6128137b5c36cfed3e904280a1
suhacanuluer <sculuer@gmail.com>
2020-11-18 21:16:23 +0300

    checkauth added

---
+++ Middleware/checkauth.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try {
+        /*JWT is send with request header!
+        Format of it: Authorization : Bearer <token>
+        */
+        const token = req.headers.authorization.split(" ")[1] || req.body.token || req.query.token;
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+        req.userData = decodedToken;  
+        next();
+    } catch (error) {
+        return res.status(401).send({
+            message: "Auth failed"
+        });
+    }
+} 
